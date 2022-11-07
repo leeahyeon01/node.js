1f891043ebb948c776293f0eae3e5345b4c38478
leeahyeon <leeahyeon01@gmail.com>
2022-11-07 20:08:49 +0900

    login

--- login/app/src/model/userStorage.js
+++ login/app/src/model/userStorage.js
@@ -1,19 +1,30 @@
-"use strict"; 
+"use strict";
 
-//클래스 생성 
-class UserStorage{
-    static #users = { 
-      //static - 인스턴스를 만들지 않고,클래스자체에서 변수에 접근하고자 할때 static을 붙여주면된다.
-      //변수에 #을 붙이면 public한 변수에서 private한 변수로 바꿔준다. 외부에서 함부러 불러올 수가 없다.
-        id:["woorimIT","나개발","김팀장"],
-        psword:["1234","1234","123456"],
-        name:["우리밋","나개발","김팀장"]
-      }; 
- 
-   static getUsers(){ //static변수를 사용하기에 static을 꼭 붙여줘야한다.
-        return this.#users;
+//클래스 생성
+class UserStorage {
+  static #users = {
+    //static - 인스턴스를 만들지 않고,클래스자체에서 변수에 접근하고자 할때 static을 붙여주면된다.
+    //변수에 #을 붙이면 public한 변수에서 private한 변수로 바꿔준다. 외부에서 함부러 불러올 수가 없다.
+    id: ["woorimIT", "나개발", "김팀장"],
+    psword: ["1234", "1234", "123456"],
+    name: ["우리밋", "나개발", "김팀장"],
+  };
+
+  //19-20강 듣기
+  static getUsers(...fields) {
+    //static변수를 사용하기에 static을 꼭 붙여줘야 반환이 된다.
+    //파라미터로 받는 값은 자동으로 배열로 받아진다 (...변수명)console.log(변수명);
+    const users = this.#users;
+
+    const newUsers = fields.reduce((newUsers, field) => {
+      //hasOwnProperty 객체가 특정 프로퍼티를 가지고 있는지를 불리언 값을 반환한다
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
       }
-      
+      return newUsers; //리턴해주면 반환값이 파라미터로 들어가게 된다⭐.
+    }, {});
+    return newUsers;
+  }
 }
 
 module.exports = UserStorage;
--- login/app/src/routes/home/home.ctroller.js
+++ login/app/src/routes/home/home.ctroller.js
@@ -1,6 +1,4 @@
-
-
-//화면 rendering 
+//화면 rendering
 const output = {
   hello: (req, res) => {
     res.render("home/index");
@@ -8,33 +6,37 @@ const output = {
   login: (req, res) => {
     res.render("home/login");
   },
-}; 
+};
 
 const process = {
   login: (req, res) => {
-//    console.log(req.body); 
+    //    console.log(req.body);
     const req_id = req.body.id,
-    req_psword = req.body.psword 
-   
-    const UserStorage = require("../../model/userStorage") //UserStorage의 클래스를 가져온다.
-    console.log(UserStorage.getUsers());
-    
-//     const response = {}; //객체 생성 
-//     //includes - 배열이 항목사이에 특정값을 포함하는지 여부를 확인하여 true/false
-//     if(user.id.includes(req_id)){ //아이디가 존재하는 지 확인
-//     //indexof - 문자열에서 특정 문자의 위치를 찾아 해당위치를 출력 
-//       const idx = user.id.indexOf(req_id); 
-//        if(user.psword[idx] === req_psword){ //아이디와 패스워드가 일치하는지 확인
-//         response.success = true;
-//         return res.json( //프론트엔드에 로그인 성공여부를 json형식으로 리턴해준다.
-//         response
-//         );
-//        }
-//     } 
-//     response.success = false; 
-//     response.msg = "로그인에 실패하였습니다."
-//     return res.json(response//response객체를 json형식으로 응답(res)하면서 return해줌
-//     );
+      req_psword = req.body.psword;
+
+    const UserStorage = require("../../model/userStorage"); //UserStorage의 클래스를 가져온다.
+
+    const users = UserStorage.getUsers("id", "psword");
+
+    console.log("aaaaaaaaaaaaaaaaaaaaaaaaa");
+    console.log(users);
+    const response = {}; //객체 생성
+    //includes - 배열이 항목사이에 특정값을 포함하는지 여부를 확인하여 true/false
+    if (users.id.includes(req_id)) {
+      //아이디가 존재하는 지 확인
+      //indexof - 문자열에서 특정 문자의 위치를 찾아 해당위치를 출력
+      const idx = users.id.indexOf(req_id);
+      if (users.psword[idx] === req_psword) {
+        //아이디와 패스워드가 일치하는지 확인
+        response.success = true;
+        return res.json(response); //프론트엔드에 로그인 성공여부를 json형식으로 리턴해준다.
+      }
+    }
+    response.success = false;
+    response.msg = "로그인에 실패하였습니다.";
+    return res.json(
+      response //response객체를 json형식으로 응답(res)하면서 return해줌
+    );
   },
 };
 
